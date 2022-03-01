3508b743aa1b75bb3301a72040645d671ed75cf7
sh6986 <okok960411@gmail.com>
2022-03-01 16:47:00 +0900

    스터디 모집글 생성 트랜잭션 설정 완료

--- project/routes/recruit.js
+++ project/routes/recruit.js
@@ -120,22 +120,22 @@ router.get('/comCd/:cgcName', async (req, res, next) => {
 /**
  * 스터디 / 스터디 모집글 생성
  */
-router.post('/', async (req, res, next) => {
-    // [TODO] 트랜잭션 처리
+ router.post('/', async (req, res, next) => {
     // [TODO] 결과값 반환 어떤식으로 하는지.. result[0]말고
     const userId = 1;   // [TODO] 로그인 구현 후 세션에서 해당아이디 가져오기
     const {srTitle, sgName, sgCnt, sgCategory, stCode, srContent} = req.body;
     let sgId;
     let insertValues = ``;
     let paramArr = [];
-    
+
+    const conn = await db.getConnection();
     try {
-        // await db.beginTransaction();
-        const result = await db.query(recruitQuery.createStudyGroup, [sgName, sgCategory, sgCnt, userId, userId]);  // 스터디그룹
+        await conn.beginTransaction();
+        const result = await conn.query(recruitQuery.createStudyGroup, [sgName, sgCategory, sgCnt, userId, userId]);  // 스터디그룹
         sgId = result[0].insertId;
         
-        await db.query(recruitQuery.createStudyRcrtm, [sgId, srTitle, srContent, userId, userId]);                  // 스터디모집글
-        await db.query(recruitQuery.createStudyMember, [sgId, userId, userId, userId]);                             // 스터디멤버
+        await conn.query(recruitQuery.createStudyRcrtm, [sgId, srTitle, srContent, userId, userId]);                  // 스터디모집글
+        await conn.query(recruitQuery.createStudyMember, [sgId, userId, userId, userId]);                             // 스터디멤버
 
         // 기술스택(stCode)을 하나 이상 선택했을때 -> 아무것도 선택하지 않으면 insert 안함
         if (stCode.length) {
@@ -147,16 +147,17 @@ router.post('/', async (req, res, next) => {
                 paramArr = paramArr.concat([sgId, userId, userId]);             // 스터디 기술스택 생성 쿼리 파라미터 (insertValues가 생기는 개수만큼 파라미터 만들기)
             });
     
-            await db.query(recruitQuery.createStudyTchsh + insertValues, paramArr);                                     // 스터디 기술스택
+            await conn.query(recruitQuery.createStudyTchsh + insertValues, paramArr);                                   // 스터디 기술스택
         } 
-        // await db.commit();
+        await conn.commit();
+        res.json({res: 'success'});
     } catch (err) {
         console.error(err);
-        // await db.rollback();
+        await conn.rollback();
         next(err);
+    } finally {
+        await conn.release();
     }
-
-    res.json({res: 'success'});
 });
 
 /**
