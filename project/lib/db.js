922731b1ee76203783f7c0d11f5c9ec7be8f51c5
sh6986 <okok960411@gmail.com>
2022-03-22 05:57:30 +0900

    배포준비

--- project/lib/db.js
+++ project/lib/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3306,
     user: 'root',
     password: process.env.DB_PASSWORD,
