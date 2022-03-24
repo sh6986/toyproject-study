57cb53517802dbf5cee642a074a4ee8e7a543021
sh6986 <okok960411@gmail.com>
2022-03-24 19:04:25 +0900

    못가져오는 정적파일 제거

--- project/app.js
+++ project/app.js
@@ -77,5 +77,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(app.get('port'), () => {
-    logger.log(app.get('port'), '번 포트에서 대기중');
+    logger.info(`${app.get('port')}번 포트에서 대기중`);
 });
