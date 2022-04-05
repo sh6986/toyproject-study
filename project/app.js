7b858bf0d2b36a3aa32ae446ae5dbbf8e82d68d3
sh6986 <okok960411@gmail.com>
2022-04-06 05:40:22 +0900

    cors 적용

--- project/app.js
+++ project/app.js
@@ -57,7 +57,10 @@ if (process.env.NODE_ENV === 'production') {
 app.use(session(sessionOption));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors());
+app.use(cors({
+    origin: 'https://codewithme.site',
+    credentials: true,
+}));
 
 app.use('/', pageRouter);
 app.use('/user', userRouter);
