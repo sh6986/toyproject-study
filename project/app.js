a118528ab7a5ea9da3acc09f426259c2f4160cf8
sh6986 <okok960411@gmail.com>
2022-04-06 06:42:04 +0900

    axios cdn 에러

--- project/app.js
+++ project/app.js
@@ -40,6 +40,7 @@ app.use(cors({
     credentials: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
