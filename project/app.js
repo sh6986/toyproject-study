0ee75305b2dbab57e492614610799d6294d58177
sh6986 <okok960411@gmail.com>
2022-04-06 06:32:14 +0900

    cors 적용

--- project/app.js
+++ project/app.js
@@ -35,7 +35,10 @@ if (process.env.NODE_ENV === 'production') {
     app.use(morgan('dev'));
 }
 
-app.use(cors({}));
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
